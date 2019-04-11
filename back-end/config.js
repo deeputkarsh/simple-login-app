603f6885f0300a02c894bdc70362c66a76c5ae84
Utkarsh Deep <utkarshdeep@outlook.com>
2019-04-11 23:45:11 +0530

    docker configs

--- back-end/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  port: 3000,
-  frontEndUrl: 'http://localhost:3015',
-  mongoUrl: 'mongodb://localhost:27017/SimpleLoginApp'
-}
--- back-end/server.js
+++ back-end/server.js
@@ -12,6 +12,8 @@ let unusedEnvVars = applicationEnvVars.filter((i) => !process.env[i])
 
 if (unusedEnvVars.length) throw new Error('Required ENV variables are not set: [' + unusedEnvVars.join(', ') + ']')
 
-const { app } = require('./src')
+const { initApp } = require('./src')
 
-app.listen(process.env.PORT, () => console.log(`Kitchen Display System running on port ${process.env.PORT}!`))
+initApp().then(app => {
+  app.listen(process.env.PORT, () => console.log(`Simple Login App running on port ${process.env.PORT}!`))
+})
